7b79c62e253bc045d9199269ca974805f5d3322c
Nidhi <nidhixpatel@gmail.com>
2019-10-03 16:10:01 -0700

    added sequence

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,25 @@
 
 
-# Time complexity: ?
+# Time complexity: ?  O(n)
 # Space Complexity: ?
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  sequence = []
+  n = num
+  while n > 0
+    sequence.push(get_newman_num(n))
+    n -= 1
+  end
+  return sequence
 end
+
+def get_newman_num(num)
+  if num < 3
+    return 1
+  else
+    num = get_newman_num(get_newman_num(num - 1)) + get_newman_num(num - get_newman_num(num - 1))
+  # P(n) = P(P(n - 1)) + P(n - P(n - 1))
+    return num
+  end
+end
+
+# puts newman_conway(20)
