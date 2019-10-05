929f61483480d76040e13815a54d6e3ccdbcfe4d
Kelly Downes <kedownes@gmail.com>
2019-10-05 11:27:16 -0700

    Implement max subarry

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,23 @@
-
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 def max_sub_array(nums)
-    return 0 if nums == nil
-    
-    raise NotImplementedError, "Method not implemented yet!"
+  return nil if nums == []
+
+  max_arr = nums[0]
+  current_max = 0
+  counter = 0
+
+  while counter < nums.length
+    current_max = current_max + nums[counter]
+    if (max_arr < current_max)
+      max_arr = current_max
+    end
+
+    if current_max < 0
+      current_max = 0
+    end
+    counter += 1
+  end
+
+  return max_arr
 end
