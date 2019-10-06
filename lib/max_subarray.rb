82071e75aa01b8846617a0e8c16f35e41a96e521
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-10-06 15:12:43 -0700

    max_subarray tests pass

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -2,7 +2,16 @@
 # Time Complexity: ?
 # Space Complexity: ?
 def max_sub_array(nums)
-    return 0 if nums == nil
-    
-    raise NotImplementedError, "Method not implemented yet!"
+  return 0 if nums == nil
+  return nil if nums.length == 0
+  max_sum = nums[0]
+  sum = 0
+
+  nums.length.times do |i|
+    sum = [nums[i], sum + nums[i]].max
+    if sum > max_sum
+      max_sum = sum
+    end
+  end
+  return max_sum
 end
