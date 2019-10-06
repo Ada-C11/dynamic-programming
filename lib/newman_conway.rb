a7512469dcd04fb2f24d2a513b6c81d3183f2daf
Amy Wyatt <amy.wyatt12@outlook.com>
2019-10-06 16:44:58 -0700

    working solution fo newman conway

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,19 @@
-
-
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: O(n) where n is the size of num
+# Space Complexity:
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError if num == 0
+
+  output = []
+  i = 1
+
+  while i <= num
+    output << conway_helper(i)
+    i += 1
+  end
+  return output.join(" ")
+end
+
+def conway_helper(num)
+  return 1 if num < 3
+  conway_helper(conway_helper(num - 1)) + conway_helper(num - conway_helper(num - 1))
 end
