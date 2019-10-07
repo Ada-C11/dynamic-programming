c02afebf9301d3b1dd5281f6063f1072ddbe33a5
Ariana Bray <ariana.bray@nordstrom.com>
2019-10-07 13:39:46 -0700

    added comments

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -5,11 +5,15 @@ def max_sub_array(nums)
     return 0 if nums.nil?
     return nil if nums.length == 0 
     
+    # could write helper method to do this also
     max_so_far = nums.min
     i = 0
+    # outer loop
     while i < nums.length
         j = i
+        # resetting max_ending_here each time we increment i 
         max_ending_here = 0
+        # inner loop
         while j < nums.length
             max_ending_here = max_ending_here + nums[j]
             if max_so_far < max_ending_here
