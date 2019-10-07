3fc3724543c91cfc0e1542c0be8c91b2bba91838
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-10-06 21:04:39 -0700

    wrote method and it passes tests

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,30 +1,25 @@
-require "pry"
-
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: O(n) where n is the input value.
+# Space Complexity: O(n) where n is the input value.
 def newman_conway(num)
   output_array = []
   if num == 0
-    raise ArgumentError("Input must be greater than 0.")
+    raise ArgumentError
   elsif num == 1
-    output_array.push(1)
+    return "1"
   elsif num == 2
-    output_array.push(1)
-    output_array.push(1)
+    return "1 1"
   else
-    output_array = [1, 1]
-    i = 2
-    while i < num
-      output_array.push(output_array[i - 1] + output_array[i - output_array[i - 1]])
+    output_array = [0, 1, 1]
+    i = 3
+    while i <= num
+      output_array.push(output_array[output_array[i - 1]] + output_array[i - output_array[i - 1]])
       i += 1
     end
   end
 
   output_string = "1"
-  if num > 1
-    output_array[1..-1].each do |num|
-      output_string += " #{num}"
-    end
+  output_array[2..-1].each do |num|
+    output_string += " #{num}"
   end
 
   return output_string
--- test/newman_conway_test.rb
+++ test/newman_conway_test.rb
@@ -12,43 +12,43 @@ describe "Newman Conway Tests" do
     expect(answer).must_equal "1 1 2 2 3 4 4 4 5 6 7 7 8"
   end
 
-  # it "works with 20" do
-  #   # Arrange
-  #   input = 20
+  it "works with 20" do
+    # Arrange
+    input = 20
 
-  #   # Act
-  #   answer = newman_conway(input)
+    # Act
+    answer = newman_conway(input)
 
-  #   # Assert
-  #   expect(answer).must_equal "1 1 2 2 3 4 4 4 5 6 7 7 8 8 8 8 9 10 11 12"
-  # end
+    # Assert
+    expect(answer).must_equal "1 1 2 2 3 4 4 4 5 6 7 7 8 8 8 8 9 10 11 12"
+  end
 
-  # it "works with base cases" do
-  #       # Arrange
-  #       input = 0
+  it "works with base cases" do
+        # Arrange
+        input = 0
 
-  #       # Act-Assert
-  #       expect {
-  #         newman_conway(input)
-  #       }.must_raise ArgumentError
+        # Act-Assert
+        expect {
+          newman_conway(input)
+        }.must_raise ArgumentError
     
 
-  #       # Arrange
-  #       input = 1
+        # Arrange
+        input = 1
 
-  #       # Act
-  #       answer = newman_conway(input)
+        # Act
+        answer = newman_conway(input)
     
-  #       # Assert
-  #       expect(answer).must_equal "1"
+        # Assert
+        expect(answer).must_equal "1"
 
-  #       # Arrange
-  #       input = 2
+        # Arrange
+        input = 2
 
-  #       # Act
-  #       answer = newman_conway(input)
+        # Act
+        answer = newman_conway(input)
     
-  #       # Assert
-  #       expect(answer).must_equal "1 1"        
-  # end
+        # Assert
+        expect(answer).must_equal "1 1"        
+  end
 end
