bee849900730dce8a83d6d043873d060c7a6a3d3
Amy Wyatt <amy.wyatt12@outlook.com>
2019-10-07 23:47:26 -0700

    faster run time

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -7,13 +7,15 @@ def newman_conway(num)
   i = 1
 
   while i <= num
-    output << conway_helper(i)
+    output << conway_helper(i, output)
     i += 1
   end
   return output.join(" ")
 end
 
-def conway_helper(num)
+def conway_helper(num, output)
   return 1 if num < 3
-  conway_helper(conway_helper(num - 1)) + conway_helper(num - conway_helper(num - 1))
+  return output[num - 1] if output[num - 1]
+  helper = conway_helper(num - 1, output)
+  conway_helper(helper, output) + conway_helper(num - helper, output)
 end
