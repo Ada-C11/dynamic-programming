1f9de743767d44ebd6f8a0fb34c4258b9bebc594
amyesh <amyesh08@gmail.com>
2019-10-07 20:45:39 -0700

    completed newman_conway method and time space complexity

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,21 @@
 
 
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: O(n)
+# Space Complexity: O(n)
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError if num == 0
+  return "1" if num == 1
+  result = "1 1"
+  n = 3
+  while n <= num
+    conway_num = newman_recursive(n)
+    result += " " + conway_num.to_s
+    n += 1
+  end
+  return result
+end
+
+def newman_recursive(n)
+  return 1 if n <= 2
+  return newman_recursive(newman_recursive(n - 1)) + newman_recursive(n - newman_recursive(n - 1))
 end
