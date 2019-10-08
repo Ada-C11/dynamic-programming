2bd6c6e45d3d01af72a2d44cb60f0d86da5a2878
Erica Norris <norrise120@gmail.com>
2019-10-07 20:06:59 -0700

    max sub array method completed

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -2,7 +2,20 @@
 # Time Complexity: ?
 # Space Complexity: ?
 def max_sub_array(nums)
-    return 0 if nums == nil
+  return 0 if nums == nil
     
-    raise NotImplementedError, "Method not implemented yet!"
+  max = nums[0]
+  current_max = 0
+  count = 0
+
+  while count < nums.length
+    current_max = current_max + nums[count]
+
+    max = current_max if max < current_max
+    current_max = 0 if current_max < 0
+
+    count += 1
+  end
+
+  return max
 end
