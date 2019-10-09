6612b7be4835aaace92e894133b7ba4066e0f14b
Amy Wyatt <amy.wyatt12@outlook.com>
2019-10-08 23:23:29 -0700

    time and space

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,30 +1,16 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n) where n is the value of nums
+# Space Complexity: O(1)
 def max_sub_array(nums)
-  return nil if nums[0] == nil
+  return 0 if nums == nil
 
-  i = 0
-  max_so_far = nums[i]
-
-  while nums[i] != nil
-    max_this_round = max_sub_array_helper(nums, i)
-    max_so_far = max_this_round > max_so_far ? max_this_round : max_so_far
-    i += 1
-  end
-  return max_so_far
-end
-
-def max_sub_array_helper(nums, i)
-  max_so_far = nums[i]
+  max_so_far = nil
   max_ending_here = 0
 
-  while nums[i] != nil
-    max_ending_here = max_ending_here + nums[i]
-    if max_so_far < max_ending_here
-      max_so_far = max_ending_here
-    end
-    i += 1
+  nums.each do |num|
+    max_ending_here += num
+    max_so_far = max_ending_here if !max_so_far || max_ending_here > max_so_far
+    max_ending_here = 0 if max_ending_here < 0
   end
   return max_so_far
 end
