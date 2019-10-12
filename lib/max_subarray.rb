cb39dd04f260151029d541ee0f560b4fbf990a62
Jansen <v.jansen.martin@gmail.com>
2019-10-12 11:35:59 -0700

    Tests passing; Add time and space complecity for 'max_sub_array'

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,37 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n), where n is the number of elements inside the nums array
+# Space Complexity: O(1), because extra space required isn't determined by input
 def max_sub_array(nums)
-    return 0 if nums == nil
-    
-    raise NotImplementedError, "Method not implemented yet!"
+  return nil if nums == []
+  return 0 if nums == nil
+
+  max_so_far = 0
+  max_ending_here = 0
+  max_element = nums[0]
+
+  i = 0
+
+  while i <= nums.length - 1
+    max_ending_here = max_ending_here + nums[i]
+
+    if max_ending_here < 0
+      max_ending_here = 0
+    end
+
+    if max_so_far < max_ending_here
+      max_so_far = max_ending_here
+    end
+
+    if max_element < nums[i]
+      max_element = nums[i]
+    end
+
+    i += 1
+  end
+
+  if max_so_far == 0
+    max_so_far = max_element
+  end
+
+  return max_so_far
 end
