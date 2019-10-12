493a453b7a5d99a1bea1f18d9910c4927da7722e
Jansen <v.jansen.martin@gmail.com>
2019-10-12 11:51:14 -0700

    All tests passing; Add space and time complexities to 'newman_conway' method

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,23 @@
 
 
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: O(n), where n is the input
+# Space Complexity: O(n), where n is the input
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError if num == 0
+  return "1" if num == 1
+  return "1 1" if num == 2
+
+  conway_numbers = Array.new(num)
+  conway_numbers[1] = 1
+  conway_numbers[2] = 1
+
+  i = 3
+  while i <= num
+    conway_numbers[i] = conway_numbers[conway_numbers[i - 1]] + conway_numbers[i - (conway_numbers[i - 1])]
+
+    i += 1
+  end
+
+  conway_numbers = conway_numbers[1..-1].join(" ")
+  return conway_numbers
 end
