8469bb8e42e621ebfaf87485eac2863d7ded579f
Amy Phung <phung.amy85@gmail.com>
2019-10-13 14:57:24 -0700

    complete max subarray method

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,23 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n) where n is the length of the input array
+# Space Complexity: O(1)
 def max_sub_array(nums)
-    return 0 if nums == nil
+  return 0 if nums == nil
     
-    raise NotImplementedError, "Method not implemented yet!"
+  max_so_far = 0
+  max = nums[0]
+  i = 0
+    
+  while i < nums.length
+    max_so_far += nums[i]
+    
+    if max_so_far > max
+      max = max_so_far
+    end
+    if max_so_far < 0
+      max_so_far = 0
+    end
+    i += 1
+  end
+  return max
 end
