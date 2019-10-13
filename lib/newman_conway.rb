490c5bafd517b671b65844395e04ed760a91dda1
Shubha Rajan <shubharajan94@gmail.com>
2019-10-12 17:42:18 -0700

    solved newman conway exercise

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -3,5 +3,11 @@
 # Time complexity: ?
 # Space Complexity: ?
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  seq = [0, 1, 1]
+  raise ArgumentError if num <= 0
+  return seq[1..num].join(' ')if num == 1 || num == 2
+  (3..num).each do |i|
+    seq[i] = seq[seq[i-1]] + seq[i - seq[i - 1]]
+  end
+  return seq[1..num].join(' ')
 end
