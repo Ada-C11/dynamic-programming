a7f65560df55d10c03df3c143b45da1903feec05
Ngoc Le <ngocle@outlook.com>
2019-10-13 23:14:26 -0700

    implemented newman_conway function

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -2,6 +2,19 @@
 
 # Time complexity: ?
 # Space Complexity: ?
-def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+
+
+def newman_conway(nums)
+  result = Array.new()
+  raise ArgumentError if nums == 0
+  return "1" if nums == 1 
+  result[1] = 1
+  result[2] = 1
+  if nums > 2
+    (3..nums).each do |num|
+    result[num] = result[result[num - 1]] + result[num - result[num - 1]]
+    end
+  end
+  result.delete_at(0)
+  return result.join(" ")
 end
