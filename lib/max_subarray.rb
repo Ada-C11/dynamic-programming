ba72fa69975862c7fd9aecc2b6a8e57873750436
Ngoc Le <ngocle@outlook.com>
2019-10-16 00:28:12 -0700

    added max_sub_array function

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -3,6 +3,22 @@
 # Space Complexity: ?
 def max_sub_array(nums)
     return 0 if nums == nil
-    
-    raise NotImplementedError, "Method not implemented yet!"
+    return nil if nums.empty?
+    max_so_far = nums.max
+    if max_so_far < 0
+        return max_so_far
+    end
+    max_ending_here = 0
+
+    nums.each do |num|
+        max_ending_here = max_ending_here + num
+
+        if max_ending_here < 0
+            max_ending_here = 0
+        end
+        if max_so_far < max_ending_here
+            max_so_far = max_ending_here
+        end
+    end
+    return max_so_far
 end
