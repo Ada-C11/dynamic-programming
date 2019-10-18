fdacf03dd65f57af24fcf18125b4e3bf717a5d5c
Kate Sandler <kate@katekatekate.net>
2019-10-17 17:43:13 -0700

    max_subarray dynamic programming

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,15 @@
+def max(num1, num2)
+    return num1 > num2 ? num1 : num2
+end
 
-# Time Complexity: ?
-# Space Complexity: ?
 def max_sub_array(nums)
     return 0 if nums == nil
-    
-    raise NotImplementedError, "Method not implemented yet!"
+
+    max_now = max = nums[0]
+
+    (1...nums.length).each do |i|
+        max_now = nums[i] > max_now + nums[i] ? nums[i] : max_now + nums[i]
+        max = max_now if max_now > max
+    end
+    return max
 end
