ae90e4cbdf7ee33dfe0aff39878d4b18fc75301e
Kim Fasbender <v-kimberlyf@zillowgroup.com>
2019-10-20 18:57:43 -0700

    implemented max_sub_array with tests passing

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,22 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n) - where n is equal to the length of nums - 1
+# Space Complexity: O(1) - constant
 def max_sub_array(nums)
-    return 0 if nums == nil
-    
-    raise NotImplementedError, "Method not implemented yet!"
+  return nil if nums == nil || nums.empty?
+
+  current_sum = nums.first
+  max = nums.first
+
+  (1...nums.length).each do |i|
+    current_sum += nums[i]
+
+    if nums[i] > current_sum
+      max = nums[i] if nums[i] > max
+      current_sum = nums[i]
+    else
+      max = current_sum if current_sum > max
+    end
+  end
+
+  return max
 end
